e0a21e450624851eb02cd2e438e508e37351ed65
fco <franciscosusana2292@gmail.com>
2019-06-12 11:53:12 +0200

    2nd cmmt, template integration 1st version

--- mensa-vue/src/components/base.vue
+++ mensa-vue/src/components/base.vue
@@ -1,17 +1,34 @@
 <template>
-    <div>
+    <div lang="zxx" class="no-js">
         Base
+        <cheader></cheader>
+        <cbanner></cbanner>
+        <cfooter></cfooter>
     </div>
 </template>
 
 <script>
+
+// Import components
+import header from './common/header.vue'
+import banner from './common/banner.vue'
+import footer from './common/footer.vue'
+
 export default {
+  components:{
+    'cheader':header,
+    'cbanner':banner,
+    'cfooter':footer,
+  },
   data: function () {
     return {
       title: 'Mensa-Online'
     }
   },
-  // Usage with context the component
+  // head of the HTML file
+
+
+
   // Meta tags
   head: {
     // To use "this" in the component, it is necessary to return the object through a function
@@ -25,7 +42,7 @@ export default {
         // <!-- Mobile Specific Meta -->
         {name: name="viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no"},
         // <!-- Author Meta -->
-		{name:"author", content:"colorlib"},
+		    {name:"author", content:"colorlib"},
         // <!-- Meta Description -->
         {name:"description", content:""},
         // <!-- Meta Keyword -->
@@ -33,16 +50,45 @@ export default {
         // <!-- meta character set -->
 		// {charset="UTF-8"},
     ],
+    // base: [
+    //     {href:"/"},
+    // ],
     // link tags
     link: [
         {href:"https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700", rel:"stylesheet"},
         {rel:"stylesheet", href:"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"},
+        
         // CSS
-        {rel:"stylesheet", href:"./static/css/main.css"}
-
+        {rel:"stylesheet", type:"text/html",href:"/common/static/css/main.css"},
+        {rel:"stylesheet", type:"text/html",href:"/common/static/css/linearicons.css"},
+        {rel:"stylesheet", type:"text/html",href:"/common/static/css/font-awesome.min.css"},
+        {rel:"stylesheet", type:"text/html",href:"/common/static/css/bootstrap.css"},
+        {rel:"stylesheet", type:"text/html",href:"/common/static/css/magnific-popup.css"},
+        {rel:"stylesheet", type:"text/html",href:"/common/static/css/jquery-ui.css"},
+        {rel:"stylesheet", type:"text/html",href:"/common/static/css/nice-select.css"},
+        {rel:"stylesheet", type:"text/html",href:"/common/static/css/animate.min.css"},
+        {rel:"stylesheet", type:"text/html",href:"/common/static/css/owl.carousel.css"},
     ],
+  
     script: [
-    //   { type: 'text/javascript', src: 'cdn/to/script.js', async: true, body: true}, // Insert in body
+      
+      {type: 'text/javascript', src: '/src/components/common/static/js/vendor/jquery-2.2.4.min.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/popper.min.js', async: true, body: true},
+      // {type: 'text/javascript', src: '/src/components/common/static/js/vendor/bootstrap.min.js', async: true, body: true},
+      // {type: 'text/javascript', src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/jquery-ui.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/easing.min.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/hoverIntent.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/superfish.min.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/jquery.ajaxchimp.min.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/jquery.magnific-popup.min.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/jquery.nice-select.min.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/owl.carousel.min.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/isotope.pkgd.min.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/mail-script.js', async: true, body: true},
+      {type: 'text/javascript', src: '/src/components/common/static/js/main.js', async: true, body: true}, // Insert in body
+    
+
     ],
     style: [
     //   { type: 'text/css', inner: 'body { background-color: #000; color: #fff}', undo: false },
---
+++ mensa-vue/src/components/common/header.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <header id="header">
+            <div class="header-top">
+                <div class="container">
+                      <div class="row justify-content-center">
+                          <div id="logo">
+                            <a href="index.html"><img src="./static/img/logo.png" alt="" title="" /></a>
+                          </div>
+                      </div>			  					
+                </div>
+            </div>
+            <div class="container main-menu">
+                <div class="row align-items-center justify-content-center d-flex">			
+                    <nav id="nav-menu-container">
+                  
+                        <ul class="nav-menu">
+                          <li><a href="index.html">Home</a></li>
+                          <li><a href="login.html">Login</a></li>
+                          <li><a href="signup.html">Signup</a></li>
+                          <li><a href="menu.html">Menu</a></li>
+						  <li><a href="preorder.html">Pre-Order</a></li>
+                         <li><a href="#wallet">Wallet</a>
+						  			<ul>
+								<li><a href="balance.html">Balance Check</a></li>								
+								<li><a href="recharge.html">Recharge</a></li>
+								<li><a href="pay.html">Payment Method</a>
+									
+								</li>
+                                    </ul>
+								</li>
+                          			
+                                <li><a href="#settings">Settings</a>
+						  			<ul>
+								<li><a href="personalinfo.html">Personal Info</a></li>								
+								<li><a href="notifications.html">Notifications</a></li>
+								<li><a href="language.html">Language</a></li>
+									<li><a href="logout.html">Log Out</a>
+								</li>
+                                    </ul>
+								</li>						  
+                          
+                        </ul>
+                      </nav><!-- #nav-menu-container -->					      		  
+                </div>
+            </div>
+        </header><!-- #header -->
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
